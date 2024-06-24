195dfabb5733d54a1d7b86e20d5f0c290aac2d11
ThiagoSausmikat <tsmoura.93@gmail.com>
2024-06-24 15:15:09 -0300

    feat: pagina sobre

--- projeto-final/App.tsx
+++ projeto-final/App.tsx
@@ -3,14 +3,13 @@ import StackComponent from './src/routes/stack';
 import { View } from 'react-native-reanimated/lib/typescript/Animated';
 import React from 'react';
 import DetalheProduto from './src/screens/DetalhesProduto';
+import { Sobre } from './src/screens/Sobre';
 
 export default function App() {
   return (
-    <>
-      <NavigationContainer>
-       <StackComponent/>
-     </NavigationContainer>
-    </>
+  <>
+    <Sobre />
+  </>
    
   );
 }
