706d1de10979db137337a2f03c0a3ae9d92b9806
Kaiky <kaikflamen@gmail.com>
2024-06-26 15:32:48 -0300

    Update: Drawer

---
+++ projeto-final/src/screens/CustomDrawer/styles.ts
@@ -0,0 +1,45 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  drawerContent: {
+    flex: 5,
+    backgroundColor: "#2F2D2D",
+  },
+
+  drawerItemList: {
+    flex: 3,
+    justifyContent: "space-around",
+  },
+
+  perfil: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  nomePerfil: {
+    color: "white",
+  },
+
+  letterCategory: {
+    padding: 10,
+  },
+
+  category: {
+    color: "white",
+    fontFamily: "System",
+    fontWeight: "bold",
+  },
+
+  bottomSair: {
+    marginBottom: 15,
+    flex: 1,
+    justifyContent: "flex-end",
+  },
+
+  sair: {
+    color: "#D92744",
+    fontFamily: "Museo-Moderno-Medium",
+    fontSize: 16,
+  },
+});
